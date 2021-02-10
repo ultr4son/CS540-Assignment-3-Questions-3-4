53fd468924c4923e166f3a47e5afa3a240df3102
Tristan Thompson <thomtris@oregonstate.edu>
2021-02-09 21:38:16 -0800

    added code template to question 3

--- Question3/main.cpp
+++ Question3/main.cpp
@@ -1,3 +1,23 @@
-int main() {
+#include <iostream>
+#include <string.h>
+
+void printUsage() {
+    std::cout << "Usage: {-C | -L <id>}" << std::endl;
+}
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        printUsage();
+    }
+    else {
+        if(strcmp(argv[1], "-C") == 0) {
+            //Put code here
+        }
+        else if(strcmp(argv[1], "-L") == 0 && argc > 2) {
+            //Put code here
+        }
+        else {
+            printUsage();
+        }
+    }
 
 }
