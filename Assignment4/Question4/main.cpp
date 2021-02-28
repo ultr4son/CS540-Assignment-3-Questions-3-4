e99bb1c7d69b7b251cb4b4afc82f86f62f6cd0ca
Andrey-Korn <andreykornilovich@gmail.com>
2021-02-28 15:14:30 -0800

    added buffer population code

--- Assignment4/Question4/main.cpp
+++ Assignment4/Question4/main.cpp
@@ -21,6 +21,28 @@ struct ReadBuffer {
     std::ifstream* file;
 };
 
+int get_eid(std::string e) {
+    int eid;
+    sscanf(e.c_str(), "%d,", &eid);
+    return eid;
+}
+
+bool compare_eid(std::string a, std::string b) {
+    int eid_a = get_eid(a);
+    int eid_b = get_eid(b);
+    return eid_a < eid_b;
+}
+
+std::string peek_line(std::ifstream& file) {
+        std::string tuple;
+        int pos = file.tellg();
+
+        getline(file, tuple);
+        
+        file.seekg(pos);
+        return tuple;
+}
+
 int to_runs(std::string record_file_name, bool (*compar)(std::string, std::string)){
     int runs_written;
 
@@ -40,7 +62,7 @@ int to_runs(std::string record_file_name, bool (*compar)(std::string, std::strin
         sort(records.begin(), records.end(), compar);
 
         // write out sorted runs
-        std::string run_file_name = "emp0" + std::to_string(runs_written) + ".csv";
+        std::string run_file_name = "emp" + std::to_string(runs_written) + ".csv";
         std::ofstream run_file(run_file_name, std::ofstream::out | std::ofstream::trunc);
         for(int w = 0; w < num_read; w++){
             run_file << records[w] << "\n";
@@ -50,29 +72,48 @@ int to_runs(std::string record_file_name, bool (*compar)(std::string, std::strin
     return runs_written;
 }
 
-bool compare_emp(std::string a, std::string b) {
-    int eid_a;
-    sscanf(a.c_str(), "%d,", &eid_a);
-
-    int eid_b;
-    sscanf(b.c_str(), "%d,", &eid_b);
-
-    return eid_a < eid_b;
+void populate_buffer(std::vector<ReadBuffer*> files, std::string base, int beg, int end){
+    // clear buffer and populate with M-1 files 
+    // free memory here before clearing!!!
+    files.clear();
+    for(int i = beg; i < end; i++){
+        files.push_back(new ReadBuffer);
+        files[i - beg]->file = new std::ifstream("emp" + std::to_string(i) + ".csv", std::ifstream::in);
+    }
 }
 
-void merge(int runs){
+bool buffer_has_records(std::vector<ReadBuffer*> files){
+    return 1;
+}
 
+void merge_runs(int runs, std::string base){
+
+    // define file buffer and iterators to keep track of beginning
+    // and end of 
+    int beg = 0;
+    int end = std::max(runs, MAX_BLOCKS - 1);
+    std::vector<ReadBuffer*> merge_files;
+    populate_buffer(merge_files, base, beg, end);
+    
+    // while(merge_files.size() > 1){
+        // populate_buffer(merge_files, base, beg, end);
+
+        // empty buffer into new runs
+        // while(buffer_has_records(merge_files)){
+
+        // }
+    // }
 }
 
 // General multi-way merge sort
 int main(int argc, char** argv){
 
     // Pass 0
-    int emp_runs = to_runs(EMP_FILE, compare_emp);
+    int emp_runs = to_runs(EMP_FILE, compare_eid);
     // std::cout << emp_runs << std::endl;
 
     // Pass i
-    merge(emp_runs);
+    merge_runs(emp_runs, "emp");
 
     return 0;
 }
